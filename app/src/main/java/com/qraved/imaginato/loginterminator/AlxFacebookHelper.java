e910069e1d31885b13a6147685f9e57335d28e3b
alex.zhuo <alex.zhuo@imaginato.com>
2016-10-17 18:41:00 +0800

    add facebook login

---
+++ app/src/main/java/com/qraved/imaginato/loginterminator/AlxFacebookHelper.java
@@ -0,0 +1,267 @@
+package com.qraved.imaginato.loginterminator;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookRequestError;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URLEncoder;
+
+
+/**
+ * Created by Alex on 2016/8/16.
+ */
+public class AlxFacebookHelper {
+    public CallbackManager facebookCallbackManager;
+
+
+    /**
+     * 初始化facebook的SDK自带按钮
+     * 这里会把点击事件拦住，不会向下层的facebook button传递,然后先执行自定义的点击事件，再执行facebook button的原生点击事件
+     * 可以在自定义点击事件里写一些记录追踪或者动画的代码
+     * @param clickView 压在facebook原生登录按钮上面的View，用来拦截点击事件,可以传null
+     * @param lbFacebook facebook原生登录按钮
+     * @param addtionalClickEvent  拦截facebook原生按钮点击事件，并附加一个点击事件，可以传null
+     */
+    public void initFBLoginButton(View clickView, final LoginButton lbFacebook, Fragment fragment, FacebookCallback<LoginResult> facebookCallback, final Runnable addtionalClickEvent){
+        initFBLoginButton(lbFacebook,fragment,facebookCallback);
+        if(clickView == null)return;
+        clickView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("AlexFB","点击了facebook上面的按钮");
+                signOut();//点击登录按钮先登出
+                if(addtionalClickEvent != null)addtionalClickEvent.run();//先执行自定义点击事件
+                lbFacebook.callOnClick();//再执行facebook登录按钮原生点击事件
+            }
+        });
+    }
+
+    /**
+     * 初始化facebook登录按钮，需要自己写一个登录回调传进来
+     * @param lbFacebook
+     * @param fragment 可以传null
+     * @param facebookCallback
+     */
+    public void initFBLoginButton(LoginButton lbFacebook, Fragment fragment, FacebookCallback<LoginResult> facebookCallback){
+        lbFacebook.setReadPermissions("public_profile", "email", "user_birthday", "user_status","user_friends");
+        // 如果你把登陆按钮放在一个fragment里面，就用这个方法，用来使用fragment启动facebook的activity并调用fragment的onActivityResult
+        if(fragment != null)lbFacebook.setFragment(fragment);
+        // Other app specific specialization
+        facebookCallbackManager = CallbackManager.Factory.create();
+        // z注册登录回调
+        lbFacebook.registerCallback(facebookCallbackManager,facebookCallback);
+    }
+
+    /**
+     * 获取facebook用户的基本信息
+     * @param callback
+     */
+    public static void getUserFacebookBasicInfo(final FacebookUserInfoCallback callback) {
+        // 获取基本文本信息
+        Log.i("AlexFB", "准备获取facebook用户基本信息");
+        if(callback == null)return;
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                if (response == null) {
+                    callback.onFailed("无法获取用户基本信息");
+                    return;
+                }
+                Log.i("AlexFB", "获取用户基本信息完毕，object是" + object);
+                JSONObject responseJsonObject = response.getJSONObject();
+                Log.i("AlexFB", "而response 的object是" + responseJsonObject);//这两个jsonObject是一样的
+                if (responseJsonObject == null) {
+                    callback.onFailed("无法获取用户基本信息2" + response.getError().getErrorType() + "   " + response.getError().getErrorMessage());
+                    return;
+                }
+                FaceBookUserInfo userInfo = new FaceBookUserInfo();
+                userInfo.id = getFacebookGraphResponseString(responseJsonObject, "id");
+                userInfo.firstName = getFacebookGraphResponseString(responseJsonObject, "first_name");
+                userInfo.lastName = getFacebookGraphResponseString(responseJsonObject, "last_name");
+                userInfo.userName = getFacebookGraphResponseString(responseJsonObject, "name");
+                userInfo.birthday = getFacebookGraphResponseString(responseJsonObject, "birthday");
+                userInfo.updateTime = getFacebookGraphResponseString(responseJsonObject, "updated_time");
+                userInfo.email = getFacebookGraphResponseString(responseJsonObject, "email");
+                userInfo.gender = getFacebookGraphResponseString(responseJsonObject, "gender");
+                callback.onCompleted(userInfo);
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link,email,first_name,last_name,gender,locale,timezone,updated_time,verified");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    /**
+     * 获取用户的token之后，根据此token联网获取该用户详细信息的回调函数
+     */
+    public interface FacebookUserInfoCallback{
+        void onCompleted(FaceBookUserInfo userInfo);//成功回调
+        void onFailed(String reason);//失败回调
+    }
+    public interface FacebookUserImageCallback{
+        void onCompleted(String imageUrl);//成功回调
+        void onFailed(String reason);//失败回调
+    }
+
+    /**
+     * 用于存储Facebook用户基本信息的类
+     */
+    public static class FaceBookUserInfo {
+        public String id;
+        public String firstName;
+        public String lastName;
+        public String userName;
+        public String birthday;
+        public String location;
+        public String updateTime;
+        public String email;
+        public String gender;
+        public String avatar;//头像url
+
+        @Override
+        public String toString() {
+            final StringBuffer sb = new StringBuffer("FaceBookUserInfo{");
+            sb.append("id='").append(id).append('\'');
+            sb.append(", firstName='").append(firstName).append('\'');
+            sb.append(", lastName='").append(lastName).append('\'');
+            sb.append(", userName='").append(userName).append('\'');
+            sb.append(", birthday='").append(birthday).append('\'');
+            sb.append(", location='").append(location).append('\'');
+            sb.append(", updateTime='").append(updateTime).append('\'');
+            sb.append(", email='").append(email).append('\'');
+            sb.append(", gender='").append(gender).append('\'');
+            sb.append(", avatar='").append(avatar).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+
+    private static String getFacebookGraphResponseString(JSONObject graphResponse, String flag) {
+        String value = "";
+        try {
+            value = graphResponse.getString(flag);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.i("AlexFB","获取用户信息 flag="+flag+"   结果是"+value);
+        return value;
+    }
+
+    /**
+     * 通过facebook的userId,联网异步获取用户的头像url
+     * @param facebookUserId
+     */
+    public static void getFacebookUserPictureAsync(String facebookUserId, final FacebookUserImageCallback callback) {
+        Log.i("AlexFB","准备获取用户头像");
+        if(callback == null || facebookUserId == null || facebookUserId.length()==0)return;
+        Bundle parameters = new Bundle();
+        parameters.putBoolean("redirect", false);
+        parameters.putString("height", "300");
+        parameters.putString("type", "normal");
+        parameters.putString("width", "300");
+        GraphRequest graphRequest= new GraphRequest(AccessToken.getCurrentAccessToken(), "/" + facebookUserId + "/picture", parameters, HttpMethod.GET, new GraphRequest.Callback() {
+            public void onCompleted(GraphResponse response) {
+                if (response == null) {
+                    callback.onFailed("获取用户facebook头像失败");
+                    return;
+                }
+
+                if (response.getError() != null) {
+                    FacebookRequestError facebookRequestError = response.getError();
+                    callback.onFailed("获取用户facebook头像失败2：：" + facebookRequestError.getErrorMessage());
+                    return;
+                }
+
+                JSONObject responseJsonObject = response.getJSONObject();
+                if (responseJsonObject == null) {
+                    callback.onFailed("获取用户facebook头像失败3");
+                    return;
+                }
+                Log.i("AlexFB", "facebook直接返回的头像信息是" + responseJsonObject.toString());
+                String avatarUrl = "";
+                try {
+                    JSONObject dataJsonObject = responseJsonObject.getJSONObject("data");
+                    avatarUrl = dataJsonObject.getString("url");
+                    avatarUrl = URLEncoder.encode(avatarUrl, "UTF-8");
+                    Log.i("AlexFB", "用户头像获取完毕 avatarUrl:" + avatarUrl);
+                } catch (Exception e) {
+                    callback.onFailed("获取用户facebook头像失败4"+e.getStackTrace().toString());
+                }
+                callback.onCompleted(avatarUrl);
+            }
+        }
+        );
+        Log.i("AlexFB","version:"+graphRequest.getVersion()+"");
+        graphRequest.executeAsync();
+    }
+
+    /**
+     * 解绑facebook
+     */
+    public static void signOut(){
+        Log.i("AlexFB","准备正式解绑本地的facebook登录信息");
+        try {
+            if (AccessToken.getCurrentAccessToken() != null) LoginManager.getInstance().logOut();//登出facebook
+        }catch(Exception ex){
+            Log.i("AlexFB","登出出现异常");
+        }
+
+    }
+
+    /**
+     * 使用facebook弹出框进行分享的代码
+     */
+    public interface ShareFacebookCallback{
+        void onSuccess();
+        void onFailure(String reason);
+    }
+
+    /**
+     * 通过facebook sdk的默认弹出框进行分享
+     * @param activity
+     */
+    public static void showFacebookShareDialog(Activity activity, String link, String description, String caption, String imageUrl, ShareFacebookCallback callback) {
+        if (activity == null) {
+            if(callback!=null)callback.onFailure("activity 为空");
+            return;
+        }
+        if (!ShareDialog.canShow(ShareLinkContent.class)) {
+            if(callback!=null)callback.onFailure("现在不能分享");
+            return;
+        }
+        try {
+            ShareDialog shareDialog = new ShareDialog(activity);
+            ShareLinkContent.Builder builder = new ShareLinkContent.Builder();
+            if(!TextUtils.isEmpty(caption)) builder.setContentTitle(caption);
+            if(!TextUtils.isEmpty(description)) builder.setContentDescription(description);
+            if(!TextUtils.isEmpty(link))builder.setContentUrl(Uri.parse(link));
+            if(!TextUtils.isEmpty(imageUrl)) builder.setImageUrl(Uri.parse(imageUrl));
+            ShareLinkContent linkContent= builder.build();
+            shareDialog.show(linkContent);
+            if(callback!=null)callback.onSuccess();
+        } catch (Exception ex) {
+            if(callback!=null)callback.onFailure(ex.getStackTrace().toString());
+        }
+    }
+}
--- app/src/main/java/com/qraved/imaginato/loginterminator/MainActivity.java
+++ app/src/main/java/com/qraved/imaginato/loginterminator/MainActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.SignInButton;
 import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
@@ -14,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_GOOGLE_PLUS = 10001;
     public static final int REQUEST_CODE_TWITTER = 140;//这个值一定一定要和TwitterAuthConfig.DEFAULT_AUTH_REQUEST_CODE一致，否则会失败。
     TwitterAuthClient customAuthClient;//如果要在工具类里面new的话，那么这个东西就要指定为static 但是他可能会内存泄漏，所以只能在这里面去new
-
+    private LoginButton lbFacebook;
+    private AlxFacebookHelper facebookHelper = new AlxFacebookHelper();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         SignInButton sign_in_button = (SignInButton) findViewById(R.id.sign_in_button);
         TwitterLoginButton loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Twitter.initTwitter(MainActivity.this, loginButton);//pay attention to init fabric in this method.
         customAuthClient = new TwitterAuthClient();
         Twitter.initCustomTwitter(MainActivity.this, textView, customAuthClient);
+        initFacebook(findViewById(R.id.iv_facebook_login));
     }
 
     @Override
@@ -43,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 Twitter.initTwitterOnActivityResult(requestCode, resultCode, data);
             }
         }
+        if(facebookHelper != null && facebookHelper.facebookCallbackManager!= null)facebookHelper.facebookCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -55,6 +64,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         GooglePlus.initGooglePlusOnstop();
+    }
+    
+    public void initFacebook(View mButton){
+        lbFacebook = new LoginButton(this);
+        facebookHelper.initFBLoginButton(mButton, lbFacebook, null, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("AlexFB","facebook登录成功");
+                if(loginResult == null) return;
+                Log.i("AlexFB","token是"+loginResult.getAccessToken());
+                for(String s:loginResult.getRecentlyGrantedPermissions()){
+                    Log.i("AlexFB","被授予的权限是::"+s);
+                }
+                getFacebookUserBasicInfo();//获取用户邮箱，姓名，头像等基本信息
+            }
+
+            @Override
+            public void onCancel() {
+                // App code  
+                Log.i("AlexFB","facebook登录被取消");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("AlexFB","facebook登录出现异常",error);
+
+            }
+
+        }, new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Alex","点击了tv");
+                //写一些其他的点击事件，比如动画，事件追踪等  
+            }
+        });
+    }
+
+    private void getFacebookUserBasicInfo(){
+        AlxFacebookHelper.getUserFacebookBasicInfo(new AlxFacebookHelper.FacebookUserInfoCallback() {
+            @Override
+            public void onCompleted(AlxFacebookHelper.FaceBookUserInfo userInfo) {
+                Log.i("Alex","获取到的facebook用户信息是:::"+userInfo);
+                getFacebookUserImage(userInfo.id);//获取用户头像
+            }
+
+            @Override
+            public void onFailed(String reason) {
+                Log.i("AlexFB","获取facebook用户信息失败::"+reason);
+                AlxFacebookHelper.signOut();
+            }
+        });
+    }
+
+    private void getFacebookUserImage(String facebookUserId){
+        AlxFacebookHelper.getFacebookUserPictureAsync(facebookUserId,new AlxFacebookHelper.FacebookUserImageCallback() {
+            @Override
+            public void onCompleted(String imageUrl) {
+                //成功获取到了头像之后
+                Log.i("Alex","用户高清头像的下载url是"+imageUrl);
+            }
+
+            @Override
+            public void onFailed(String reason) {
+                AlxFacebookHelper.signOut();//如果获取失败了，别忘了将整个登录结果回滚
+                Log.i("AlexFB",reason);
+            }
+        });
     }
 
 }
